50228061bf2d1171fb26f8cdcc6148e010c65fa5
Maria <103320974+mariafiorio@users.noreply.github.com>
2024-12-04 20:58:27 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function validaFormulario(){  
+ if(document.getElementById("nome").value != "" &&
+    document.getElementById("email").value != "" ){
+      alert("Prontinho! você receberá as novidades por email.")
+  }else{
+      alert("Por favor, preencha os campos nome e email.")
+  }
+}
